5c6674b2056a13819f3b12a164b69a98c230819b
Maeda Rikiya <91124880+Rikiya-0427@users.noreply.github.com>
2023-05-25 11:49:01 +0900

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function onSubmit(){
-    const text = document.getElementById('answer').value;
-
-    const msg = "【送信内容】\n" + text;
-    console.log(msg);
-    sendText(msg);
-
-    return false;
-}
